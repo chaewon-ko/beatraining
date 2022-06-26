13cf2a1b303c31aad404d7aa6e396fe78426513b
pendant_k <behind8486@gmail.com>
2022-06-26 16:49:23 +0900

    Add ranking route

--- backend/beatraining/main/urls.py
+++ backend/beatraining/main/urls.py
@@ -2,5 +2,6 @@ from django import views
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('ranking', views.ranking)
 ]
