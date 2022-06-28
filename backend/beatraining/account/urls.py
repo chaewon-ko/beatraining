e089f3e27eeef8228b569d35cb90e187ca4928cd
justdo-kwon <justdokwon@naver.com>
2022-06-28 23:21:24 +0900

    loginviews

--- backend/beatraining/account/urls.py
+++ backend/beatraining/account/urls.py
@@ -1,6 +1,8 @@
 from django import views
 from django.urls import path
+from account import views
 from . import views
 urlpatterns = [
-    path('', views.account)
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
