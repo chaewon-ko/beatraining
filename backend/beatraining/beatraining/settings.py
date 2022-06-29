900195c6775eb3f4364e215f3cb1988c6085428a
pendant_k <behind8486@gmail.com>
2022-06-29 17:01:20 +0900

    Add login function

--- backend/beatraining/beatraining/settings.py
+++ backend/beatraining/beatraining/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'scheduler',
-    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+LOGIN_REDIRECT_URL = "/"
