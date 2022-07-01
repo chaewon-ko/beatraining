5b8543257cadbc91469b847c0c4800420b4f0de3
pendant_k <behind8486@gmail.com>
2022-07-01 13:56:12 +0900

    almost done

--- backend/beatraining/account/views.py
+++ backend/beatraining/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import auth
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -8,11 +8,12 @@ from . import urls
 
 def login(request):
     if request.method == 'POST':
+        print(request.POST)
         userid = request.POST['username']
         pwd = request.POST['password']
-        user = auth.authenticate(request, username=userid, password=pwd)
+        user = authenticate(request, username=userid, password=pwd)
         if user is not None:
-            auth.login(request, user)
+            login(request, user)
             return redirect('home_logout')
         else:
             return redirect('home_logout')
@@ -22,11 +23,22 @@ def login(request):
 
 
 def logout(request):
-    auth.logout(request)
-    return redirect('home')
+    logout(request)
+    return redirect('home_logout')
+
 
 def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['confirm']:
+            user = User.objects.create_user(
+                username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/signupdone')
+            else:
+                return render(request, 'signup.html')
     return render(request, 'signup.html')
+
 
 def signup_done(request):
     return render(request, 'signup_done.html')
