a4b40d1b212f86023184541c67bdc1b3056f1249
pendant_k <behind8486@gmail.com>
2022-07-02 14:56:39 +0900

    Fix

--- backend/beatraining/account/views.py
+++ backend/beatraining/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -29,14 +29,9 @@ def logout(request):
 
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password'] == request.POST['confirm']:
-            user = User.objects.create_user(
-                username=request.POST['username'], password=request.POST['password'])
-            if user is not None:
-                login(request, user)
-                return redirect('/signupdone')
-            else:
-                return render(request, 'signup.html')
+        return redirect('signup_done')
+    else:
+        return render(request, 'signup.html')
     return render(request, 'signup.html')
 
 
